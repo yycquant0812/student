d4b3d19301884550dc1d3b71994401790d589c80
yycquant0812 <xherobruce@gmail.com>
2025-10-27 16:15:35 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int age = 37;
+    double height = 169.5;
+    std::string name = "布魯斯yang";
+    bool is_student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << height << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_student << std::endl;
+
+     
+    return 0;
+}
