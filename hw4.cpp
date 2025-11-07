778b56c531f12b8a89b0988c705b33f710100eaf
yycquant0812 <xherobruce@gmail.com>
2025-11-07 16:00:33 +0800

    hw4

---
+++ hw4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x1 = 5 + 3;
+    int x2 = 5 - 3;
+    int x3 = 5 * 3;
+    int x4 = 5 / 3;
+    double x5 = 5 / 3.0;
+    int x6 = 5 % 3;
+    std::cout << x1 << " " << x2 << " "<< x3 << " "<< x4 << " "<< x5 << " "<< x6 ;
+    return 0;
+}
